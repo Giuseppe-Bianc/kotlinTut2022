f69a523fa85ebe63d645409e266535db45ab9207
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2022-12-23 18:57:47 +0100

    short max min value

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,6 @@ fun main(args: Array<String>) {
     val num = 32
     println("int Max size ${Int.MAX_VALUE}")
     println("int Min size ${Int.MIN_VALUE}")
+    println("short Max size ${Short.MAX_VALUE}")
+    println("short Min size ${Short.MIN_VALUE}")
 }
