921af3fa57f0c93b45ead3de9faeaaa1fdda6bb7
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2022-12-23 19:03:41 +0100

    long max min value

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ fun main(args: Array<String>) {
     println("Hello World!")
     println("Program arguments: ${args.joinToString()}")
     val num = 32
+    println("long Max size ${Long.MAX_VALUE}")
+    println("long Min size ${Long.MIN_VALUE}")
     println("int Max size ${Int.MAX_VALUE}")
     println("int Min size ${Int.MIN_VALUE}")
     println("short Max size ${Short.MAX_VALUE}")
